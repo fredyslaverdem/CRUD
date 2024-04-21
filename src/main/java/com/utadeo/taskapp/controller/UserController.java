542fe0c6ebfdb611f887e4df8239a55ff6cfc699
Fredy Laverde <fredys.laverdem@utadeo.edu.co>
2024-04-20 20:20:18 -0500

    Metodos agrados

--- src/main/java/com/utadeo/taskapp/controller/UserController.java
+++ src/main/java/com/utadeo/taskapp/controller/UserController.java
@@ -9,12 +9,13 @@ import com.utadeo.taskapp.service.IUserService;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 
 @RestController
 @RequestMapping("user")
@@ -35,10 +36,34 @@ public class UserController {
             "EL usuario ha sido creado",
             userStore
         );
-    }   
-    
+    }
+
+    @GetMapping
+    public List<User> index(){
+        return this.userService.getAllUsers();
+    }
+
     @GetMapping("/{id}")
     public User show(@PathVariable("id") Long id){
-        return this.userService.getById(id);
+        return this.userService.getUserById(id);
+    }
+
+    @PutMapping("/{id}")
+    public ApiResponse update(@PathVariable("id") Long id, @RequestBody User user){
+        User userUpdated = this.userService.updateUser(id, user);
+        return new ApiResponse(
+            HttpStatus.OK.value(),
+            "El usuario ha sido actualizado",
+            userUpdated
+        );
+    }
+
+    @DeleteMapping("/{id}")
+    public ApiResponse destroy(@PathVariable("id") Long id){
+        this.userService.deleteUser(id);
+        return new ApiResponse(
+            HttpStatus.NO_CONTENT.value(),
+            "El usuario ha sido eliminado"
+        );
     }
 }
